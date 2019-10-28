5ee42a0315eacc747801fc0b01d9e2053aca4d1d
Jorge Hortelano <jhortelano@biit-solutions.com>
2019-10-28 12:56:07 +0100

    Added character age to description

--- src/test/java/com/softwaremagico/tm/characters/CustomCharacters.java
+++ src/test/java/com/softwaremagico/tm/characters/CustomCharacters.java
@@ -179,7 +179,8 @@ public class CustomCharacters {
 
 		setNames(characterPlayer);
 
-		String description = characterPlayer.getCompleteNameRepresentation()
+		String description = characterPlayer.getCompleteNameRepresentation() + " (" + characterPlayer.getInfo().getAge()
+				+ " años)"
 				+ " viste con pantalones cortos, una camiseta de algún grupo de rock olvidado y una gorra del viejo y nostálgico Space Invaders que no llega a ocultar sus ojos de origen asiático. Se dedica profesionalmente a la seguridad informática y aunque no lo dice, por la seguridad con la que habla hace evidente que es bueno en ello. Ha trabajado para multiples empresas muy conocidas del sector, pero actualmente se ha hecho autónomo y tiene una empresa propia localizada en un sótano de la ciudad que llama «El Búnker». ";
 		String background = "Durante la última semana, te has desplazado a tu empresa personal para intentar acceder al exterior a pesar del corte de comunicaciones que ha sufrido la ciudad. Después de cierto esfuerzo y de utilizar una radio militar como repetidor de datos, has logrado conectar brevemente con el mundo exterior. Para tu sorpresa, parece que el mundo exterior ignora lo que está sucediendo en la ciudad. Los noticiarios y periódicos no dedican ni una sola palabra a lo ocurrido y solamente en un canal oscuro de rusia has encontrado especulaciones al respecto. En la última visita a El Bunker, casi te has cruzado con un peloton de militares. No te vieron por los pelos, ya que estaban ocupados persiguiendo a otro pobre desgraciado que deambulaba por las calles. El ruido de disparos de metralleta te obligó a ocultarte en el bunker, y no te atreviste a salir hasta dos horas después. El cuerpo sin vida de aquel desgraciado te convenció para no romper el toque de queda nunca más. ";
 		String specialRules = "Dispones de un ordenador portátil modificado que lo hace muy especial. Una gran variedad de software, puertos y cables te permiten conectarte a cualquier sistema informático al que tengas acceso físico, por lo que gracias a tus habilidades en la materia, puedes comprometer casi cualquier sistema con el tiempo necesario. No tienes penalización alguna a las tiradas de hacking siempre que puedas conectar directamente tu equipo al equipo que quieras atacar. ";
@@ -282,7 +283,8 @@ public class CustomCharacters {
 
 		setNames(characterPlayer);
 
-		String description = characterPlayer.getCompleteNameRepresentation()
+		String description = characterPlayer.getCompleteNameRepresentation() + " (" + characterPlayer.getInfo().getAge()
+				+ " años)"
 				+ " es un piloto de guerra ya retirado. En sus buenos tiempos, fue capitán de un gran bombardero de cuatro motores capaz de llevar cientos de toneladas de destrucción, una de las naves insignia de algún país del norte de Europa. Aunque le gusta jactarse de su formación militar, nunca ha vivido tiempos de guerra y por tanto, su experiencia se basa en maniobras de combates y simuladores virtuales. Después de muchas horas de preparación, su carrerá se vio truncada debido a un un estúpido accidente en unas maniobras militares. En dicho accidente perdió una pierna, lo que le obligó a retirarse. A pesar de su edad, tiene una forma física envidiable, únicamente mermada por su afición a los puros. ";
 		String background = "Tu vieja radio militar no te ha dado noticias de lo que está ocurriendo. Saliste a la calle varias veces para presentarte voluntario para ayudar al ejército, pero solo conseguiste empujones y malos modos que te devolvieron a tu casa. Hace dos días, oistes disparos cerca de tu casa. Cogiste tu arma y te asomaste. Una ráfaga de metralleta te obligó apensártelo dos veces. Solo gracias a recitar en voz alta un viejo código númerico militar hizo que los militares no incendiaran tu casa con un lanzallamas. Aún estás esperando una explicación al respecto...";
 		String specialRules = "Dispones de un código de emergencia que puedes utilizar con los militares. Si comunicas este código a un alto rango, te permite demostrar que has sido militar anteriormente, lo que te permite ganar cierta confianza con ellos y quizá sacarte de algún apuro llegado el caso. ";
@@ -379,7 +381,8 @@ public class CustomCharacters {
 
 		setNames(characterPlayer);
 
-		String description = characterPlayer.getCompleteNameRepresentation()
+		String description = characterPlayer.getCompleteNameRepresentation() + " (" + characterPlayer.getInfo().getAge()
+				+ " años)"
 				+ " es una persona devota y de las que coloca la religión como el centro de todo lo que hace. Un corte de pelo muy sencillo resalta sobre su pelo rubio y liso y su tez pálida. Atuendos sencillos pero resistentes caracterizan a una mujer que prefiere lo práctico a la apariencia. Gracias a sus fuertes creencias, la religión le permite prácticamente justificar todos lo que ocurre a su alrededor, por lo que no necesita nada más; lo que le otroga una vida serena y tranquila debido a ello. Ha visto -o cree haber visto- milagros delante suyo, lo que le demuestra que El Hacedor está siempre con ella y que todo tiene un plan divino. ";
 		String background = "A pesar del toque de queda, te has dedicado a repartir consuelo por el barrio. Has visitado vecinos asustados a los que has invitado a rezar para recobrar el coraje y la fe. Durante la última noche te cruzaste con una persona poseída por el mismísimo demonio. Fuiste atacada y solo gracias a tu dios fuiste rescatada por un militar que patrullaba la zona. El militar te detuvo y te iba a encarcelar por violar el toque de queda, pero un grupo de atacantes os sorprendió desde la oscuridad de la noche y solo pudiste correr sin mirar atrás. ";
 		String specialRules = "En situaciones sobrenaturales y siempre que hagas una interpretación adecuada, puedes intercambiar el valor de Voluntad con el de Fe. Por ejemplo, puedes recitar pasajes del libro sagrado para reconfortarte y pasar una tirada de miedo, o repetir en voz alta que el creador te guía y superar una tirada para mantener los nervios. ";
@@ -459,7 +462,8 @@ public class CustomCharacters {
 
 		setNames(characterPlayer);
 
-		String description = characterPlayer.getCompleteNameRepresentation()
+		String description = characterPlayer.getCompleteNameRepresentation() + " (" + characterPlayer.getInfo().getAge()
+				+ " años)"
 				+ " es una boxeadora profesional de la vieja Rusia. Ganadora de una medalla de plata en las olimpiadas de Budapest 2036. Tiene una altura y corpulencia anormal para su género, lo que le convierte una boxeadora temible. Por desgracia, no tiene la misma soltura fuera del ring que dentro de este, lo que hace pensar que eres algo patosa. ";
 		String background = "Tus contactos con la madre patria -una entrañable pareja de ancianitos- te han sugerido que esperases durante estos días. Hoy te tenías que reunir con ellos para obtener un pendrive con los colores de la bandera de Rusia (blanco, rojo y azul), en cuyo interior contenía encriptado información importante de aquello que estaba pasando. Si bien, al entrar en su casa has encontrado una carnicería. Un psicópata loco se te ha avalanzado encima y apenas has tenido tiempo de propinarle un gancho a la mandibula. Asombrosamente, solo se ha quedado un poco aturdido para saltar sobre ti minutos después. Lograste empujarlo de una patada y salir corriendo del edificio para encontrarte con un autobús en el que iban colocando a los vecinos. No has tenido más opción que entrar en el autobús y abandonar el pendrive a su suerte. Quien sabe, tienes otro idéntico en el que igual puedes guardar alguna información útil en el futuro. ";
 		String specialRules = "Amas a Rusia por encima de todo. Todo aquellos que hagas directamente por Rusia te da un bonificador de +2 a Voluntad. ";
@@ -544,7 +548,8 @@ public class CustomCharacters {
 
 		setNames(characterPlayer);
 
-		String description = characterPlayer.getCompleteNameRepresentation()
+		String description = characterPlayer.getCompleteNameRepresentation() + " (" + characterPlayer.getInfo().getAge()
+				+ " años)"
 				+ " es una amante de los coches, los motores y su velocidad. Se mueve en un mundo de carreras, apuestas y diversión. Cuando no está corriendo, pasa el tiempo ayudando a su familia en un taller mecánico, profesión que se le da bien y que le ha proporcionado una interesante cantidad de dinero. Su aspecto recuerda a la típica chica de barrio obrero de alguna pequeña ciudad del mediterráneo venida a menos. El pelo desaliñado, sus manos callosas y la falta de maquillaje indica que claramente prefiere a los coches que a los chicos. ";
 		String background = "Durante la última semana te has ido reuniendo con tu «familia», que es como llamas a la banda organizada que regenta el viejo taller mecánico de un barrio periférico. Cada noche te has ido reuniendo con ellos para ver, como cada vez menos de la banda aparecían. Nadie conoce el motivo del toque de queda, pero desde luego, ha sido muy malo para los negocios. No has tenido ningún encargo de aquellos que te dan mucho dinero, es decir, transportar cocaína con tu coche por todos los barrios de la ciudad. No te has atrevido a mover tu deportivo del garaje, pero durante los viajes hasta el taller, has visto manchas de sangre por las aceras. ";
 		String specialRules = "Tienes una adicción fuerte a la cocaína. Necesitas tomar una dosis diaria antes de finalizar el día o el día siguiente tendrás un -2 a todas las tiradas. Tomar una dosis necesita 30 minutos de tranquilidad. Actualmente llevas encima un total de 10 dosis. ";
@@ -630,7 +635,8 @@ public class CustomCharacters {
 
 		setNames(characterPlayer);
 
-		String description = "A " + characterPlayer.getCompleteNameRepresentation()
+		String description = "A " + characterPlayer.getCompleteNameRepresentation() + " ("
+				+ characterPlayer.getInfo().getAge() + " años)"
 				+ " le gustó desde siempre la medicina, si bien finalmente optó con ser veterinaria. Hija de inmigrantes del norte de África logró una plaza en una de las más prestigiosas universidades del país. Como amante de los animales vive con varios de ellos en su casa. Tiene una colección de gatos, lagartos y aves exóticas que convierten su casa en un auténtico circo. Se te da bien tu profesión, y casi prefieres la vida de un animal sobre la de una persona. ";
 		String background = "Como científica que eres has intentado buscar respuestas a lo que ha ocurrido en la ciudad. Lo que te ha llevado a casi ser descubierta por un gran número de militares que se atrincheraban en la barriada. Has escapado a duras penas arrastrándote por los suelos y has acabado en una calle llena de sangre reseca. Te has llevado muestras para analizar y has descubierto que la sangre es siempre humana, y desde entonces, has caído en la cuenta de que ya no quedan animales en la ciudad. Hasta las ratas os han abandonado a vuestra suerte. ";
 		String specialRules = "Tienes un buen trato con los animales. Entiendes sus necesidades casi al instante y eres capaz de cuidarlos como solo ellos necesitan. Lo que te labra una buena amistad con ellos. ";
@@ -723,7 +729,8 @@ public class CustomCharacters {
 
 		setNames(characterPlayer);
 
-		String description = characterPlayer.getCompleteNameRepresentation()
+		String description = characterPlayer.getCompleteNameRepresentation() + " (" + characterPlayer.getInfo().getAge()
+				+ " años)"
 				+ " es un joven amante de los juegos de rol que ha pasado demasiadas horas encerrado entre cuatro paredes, tal como demuestra su piel pálidad que destaca sobre su pelo oscuro. Gracias a las horas inmersas jugando al D&D 6rd Edition es capaz de ver lo que ha pasado con otros ojos. Para él, esto no es más que otra aventura como las que ya ha vivido entre papeles y lápices, que además permitirá probar tu amor por las armas.";
 		String background = "Como estudiante en una ciudad sin familia te has visto enclaustrado y solo en tu miserable piso de alquiler. Todo lo que has oído se parece mucho a lo que describen tus libros de rol, lo que te ha ayudado en parte a sobrevivir. Cuando esta mañana reventó la puerta de tu casa ese vecino completamente loco y furioso, supiste que tu única salida era usar un cebo. Por lo que saltaste de tu balcón al balcón contiguo de esa pareja de ancianitos que te trataban tan bien. Esperaste escondido en un armario hasta encontrar tu oportunidad. En la oscuridad, encontraste un extraño pendrive de colores blanco, rojo y azul, e instintivamente lo metiste en uno de tus bolsillos ¡Estamos en una película de espías!. Unas horas después la puerta de la casa se abrió y el atacante se distrajo con lo que parecía una persona bastante corpulenta, lo que te permitió escabullirte sin ser visto, y para cuando pudiste escapar del edificio, un autobús te estaba esperando rodeado de militares.";
 		String specialRules = "Siempre que quieras obtener información sobre una criatura fantástica y puedas consultar alguno de tus libros de rol puedes hacer una tirada de Saber. Según los PV obtenidos el Director de Juego de aportará más o menos información. Únicamente puedes realizar una tirada por pregunta. ";
@@ -818,7 +825,8 @@ public class CustomCharacters {
 
 		setNames(characterPlayer);
 
-		String description = characterPlayer.getCompleteNameRepresentation()
+		String description = characterPlayer.getCompleteNameRepresentation() + " (" + characterPlayer.getInfo().getAge()
+				+ " años)"
 				+ " es un hombre con buena presencia y saber vestir. Trabaja como profesor de instituto y hace gala de conocimientos en varias materias. Tiene algo que le hace especial, la gente le respeta como profesional y los que le conocen le tratan bien sin saber por qué. Es una persona con carisma y buena presencia, potenciando por su prática regular de esgrima. Gracias a su aspecto físico, su pelo castaño y sus ojos verdes típicos de la costa norte del Mediterráneo, resulta evidente que incluso en las situaciones más complicadas, cae en gracia a las mujeres. ";
 		String background = "Durante estos últimos días, has burlado el toque de queda para visitar a una de tus alumnas con las que tienes una relación especial (y algo prohíbida). Debido a tus andanzas nocturnas fuiste detenido hace dos noches por una mujer soldado, si bien pudiste convencerla de que te dejara ir. Durante las pocas horas de duró tu captura, lograste escuchar una conversación por radio en la que quedaba evidente que el ejército estaba reagrupándose en el Parque Tecnológico de la ciudad y se estaban fortificando allí para poder contener algún tipo de extraño peligro.";
 		String specialRules = "Siempres que trates con alguien del sexo opuesto, tiene una dificultad de -4 a cualquier tirada que sea para oponerse a una acción contra ti. Esta penalización únicamente se aplica a aquellas mujeres que no tengan una mala relación contigo.";
@@ -905,8 +913,9 @@ public class CustomCharacters {
 
 		setNames(characterPlayer);
 
-		String description = characterPlayer.getCompleteNameRepresentation()
-				+ " es una mujer algo entrada en años que se gana la vida prediciendo el futuro con las cartas. Ha viajado de una parte a otra del mundo con sus artes y dice que ha ayudado a grandes estrellas de Hollywood para llegar hasta lo más alto gracias a sus dotes adivinatorios. Lleva numerosas pulseras y collares con multitud de piedras preciosas y semipreciosas que junto a sus rasgos del norte de áfrica le dan un toque éxotico que le ensalza su imagen de adivina.";		
+		String description = characterPlayer.getCompleteNameRepresentation() + " (" + characterPlayer.getInfo().getAge()
+				+ " años)"
+				+ " es una mujer algo entrada en años que se gana la vida prediciendo el futuro con las cartas. Ha viajado de una parte a otra del mundo con sus artes y dice que ha ayudado a grandes estrellas de Hollywood para llegar hasta lo más alto gracias a sus dotes adivinatorios. Lleva numerosas pulseras y collares con multitud de piedras preciosas y semipreciosas que junto a sus rasgos del norte de áfrica le dan un toque éxotico que le ensalza su imagen de adivina.";
 		String background = "Las desgracias son siempre buenas para el negocio y a pesar del toque de queda inicialmente tu número de clientes aumentó. Esto ha ido en declive, si bien, gracias al contacto que has tenido con esta gente los últimos días, has escuchado numerosos rumores. Parece que una entidad oscura está detrás de todo esto y que los árboles negros son un puente entre el infierno y nuestro mundo. ";
 		String specialRules = "Eres una maestra de las cartas. Las mueves delante de los ojos de las demás personas con una agilidad asombrosa. Esto provoca una distracción ideal para encubrir tus artimañas. Siempre que utilices tus cartas como cebo, cualquier persona que te esté prestando atención quedará cautivada por tus palabras y movimientos, lo que los distraerá de cualquier otra acción causando un -2 a Percepción mientras dure el engaño.";
 		String secondaryObjectives = "Siempre has sido una timadora y una ladrona, pero los eventos actuales te dan que pensar que algo sobrenatural existe en la ciudad. Has intentado buscar qué o quién está detrás de todo esto, pero a pesar de tus habilidades de sigilo, la fuerte presencia militar te ha impedido descubrir nada. Notas que hay una presencia oscura en la ciudad, y quieres contactar con ella. Te gustaría pactar o controlar lo que sea que está pasando aquí para poder ser por fin una auténtica hechicera. ";
@@ -1002,7 +1011,8 @@ public class CustomCharacters {
 
 		setNames(characterPlayer);
 
-		String description = characterPlayer.getCompleteNameRepresentation()
+		String description = characterPlayer.getCompleteNameRepresentation() + " (" + characterPlayer.getInfo().getAge()
+				+ " años)"
 				+ " es un actor que antaño tuvo un gran éxito por participar en varias películas y series con un gran éxito. Ya retirado debido a su edad de 78 años, se compró una casita en las afueras de Habitax para vivir lejos de la fama y del ruido de Hollywood. Lo que no esperaba, que su tranquilidad se volviera en tragedia. ";
 		String background = "Tu veganismo te ha metido en serias dificultades, ya que la verdura fresca ha sido extremadamente difícil de conseguir. Has tenido que pulular por las sombras de las noches para intentar saquear fruterías o tiendas de comestibles. Has tenido mucha suerte, ya que los militares parecían siempre muy entretenidos persiguiendo a otros objetivos. Durante una de tus incursiones, has encontrado entre unas lechugas unos zarcillos negros que te has llevado a casa. No parecían comestibles pero has descubierto que crecen a un ritmo vertiginoso. Parecían sentirse atraídos por tu viejo radiocasette y en dos noches han crecido lo suficiente para enrollarse alrededor de este.";
 		String specialRules = "Pasar desapercibido. Ya sea por los papeles que ha representado, o simplemente por un golpe de suerte, siempre has sido un superviviente nato. Cada vez que haya un encuentro fortuito o alguna criatura ataque al azar a un miembro del grupo, si este personaje obtiene en una tirada un resultado de 13 o menos, será ignorado como víctima y la criaturá habrá decidido que el personaje no resulta peligroso, atacarando a otro miembro del grupo (al jugador de su derecha o izquierda). No tiene efecto si el ataque es dirigido expresamente hacia él (como por ejemplo, si la criatura se está defendiendo de los ataques de "
